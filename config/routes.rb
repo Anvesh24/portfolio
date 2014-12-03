a4b7abe9631cc6ea91a8b4af868a1c8fe3e0f605
Anvesh <anvesh.v@gmail.com>
2014-12-03 16:02:13 +0530

    small change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   get 'yoga' => 'static_pages#yoga'
 
-  get 'contact' => 'contacts#new'
+  get 'contacts' => 'contacts#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
